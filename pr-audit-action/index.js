b1ad3d763923293d67315466c846dae6ec7f5e4b
Ellington, Taylor <taylor.ellington@intel.com>
2023-01-06 11:22:30 -0800

    checking tags

--- pr-audit-action/index.js
+++ pr-audit-action/index.js
@@ -7,6 +7,22 @@ try{
 
     console.log(payload.data)
 
+    var publicRN = false;
+    var noRN = false
+    for( label in payload.data){
+        if(label.name == "Public Release Notes"){
+            publicRN = true;
+        }
+
+        if(label.name == "No Release Notes"){
+            noRN = true;
+        }
+    }
+
+    if(!publicRN && !noRN){
+        console.log("no tags! failed!")
+    }
+
 
 } catch ( error ){
     core.setFailed(error.message)
